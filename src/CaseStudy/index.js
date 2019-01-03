bca8da50b78c29e5e3407170e8094ad8d62ffa68
Stephanie Unna <steph.unna@gmail.com>
2019-01-03 23:20:03 +0000

    Adding CaseStudy component

---
+++ src/CaseStudy/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './CaseStudy.scss';
+
+class CaseStudy extends Component {
+  render() {
+    return(
+      <div className='main'>
+        here it is
+      </div>
+    )
+  }
+}
+
+export default CaseStudy;
