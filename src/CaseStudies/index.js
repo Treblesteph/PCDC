a1d3ab73ed7dc6b21323fa13f882d8ddba7778ba
Stephanie Unna <steph.unna@gmail.com>
2019-01-03 23:21:19 +0000

    Starting to parse casestudy content

--- src/CaseStudies/index.js
+++ src/CaseStudies/index.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
+import CaseStudy from '../CaseStudy'
 import './CaseStudies.css';
 
 class CaseStudies extends Component {
-  render() {
+
+  componentDidMount () {
+    this.createStudies()
+  }
+
+  createStudies = () => {
+    const { content } = this.props
+    if (content) {
+      content.map(study => {
+        const title = study.title
+        const profile = study.profileImage.fields.file.url
+        const summary = study.summary
+        const text = study.paragraph.content
+        return (
+          <CaseStudy
+            title={title}
+            profile={profile}
+            summary={summary}
+            text={text}
+          />
+        )
+      })
+    }
+  }
+
+  render() {    
     return(
       <div>Hello here is CaseStudies</div>
     )
